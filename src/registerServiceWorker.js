760233258126d39141ebb9d20a8edc1efe19026a
altenull <altenull@gmail.com>
2018-02-20 20:57:43 +0900

    added serviceWorker register status log

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -49,6 +49,8 @@ export default function register() {
         registerValidSW(swUrl);
       }
     });
+  } else {
+    console.log("won't register serviceWorker. Current env:", process.env.NODE_ENV);
   }
 }
 
@@ -56,6 +58,7 @@ function registerValidSW(swUrl) {
   navigator.serviceWorker
     .register(swUrl)
     .then(registration => {
+      console.log('serviceWorker is registered');
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
         installingWorker.onstatechange = () => {

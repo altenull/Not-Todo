e862c78e28499130ebf09fae06bb3fc11e758a82
altenull <altenull@gmail.com>
2018-02-22 19:37:16 +0900

    separated muiTheme

---
+++ src/lib/muiTheme.js
@@ -0,0 +1,13 @@
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { orange500 } from 'material-ui/styles/colors';
+
+export const muiTheme = getMuiTheme({
+  palette: {
+  },
+  appBar: {
+    color: orange500
+  },
+  floatingActionButton: {
+    color: orange500
+  }
+});

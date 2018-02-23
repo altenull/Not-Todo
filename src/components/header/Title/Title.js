d1471339d7aae118a44880e897ed98baa5598b14
altenull <altenull@gmail.com>
2018-02-23 23:01:17 +0900

    deleted calcel.svg

--- src/components/header/Title/Title.js
+++ src/components/header/Title/Title.js
@@ -4,13 +4,10 @@ import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
-// @TODO
-// 반응형 작업.
 const Title = () => {
   return (
     <div className={cx('container')}>
       <span>Not Todo</span>
-      <img src={require('static/images/cancel.svg')} height='25' alt='cancel'/>
     </div>
   );
 }

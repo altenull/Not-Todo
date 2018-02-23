b28966cdc46da1f04d9d7d23e8cc451f7538f71e
altenull <altenull@gmail.com>
2018-02-23 11:30:17 +0900

    optimized LazyDrawer component & container

--- src/components/header/LazyDrawer/LazyDrawer.js
+++ src/components/header/LazyDrawer/LazyDrawer.js
@@ -1,45 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Drawer, MenuItem } from 'material-ui';
+import { ClearIcon } from 'components/common/Icons';
 
-// @TODO
-// Clear List 클릭하면 아이템 모두 삭제하기.
 type Props = {
   open: boolean,
   onRequestChange(): void,
-  onClick(): void,
-  onMounted(): void
+  onClearClick(): void
 }
 
-class LazyDrawer extends Component<Props> {
-  componentDidMount() {
-    const { onMounted } = this.props;
+const LazyDrawer = ({open, onRequestChange, onClearClick}: Props) => {
+  const clearIcon = <ClearIcon />;
 
-    let frameCount = 0;
-    const open = () => (frameCount++ > 0) ? onMounted() : requestAnimationFrame(open);
-    requestAnimationFrame(open);
-  }
-
-  render() {
-    const { open, onRequestChange, onClick } = this.props;
-
-    return (
-      <Drawer
-        docked={false}
-        width={200}
-        open={open}
-        onRequestChange={onRequestChange}
-      >
-        <MenuItem
-          primaryText={'Clear List'} // @TODO 아이콘 어떻게 할건지. Material Icon SVG 참고.
-          leftIcon={<svg fill="#757575" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-          <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
-          <path d="M0 0h24v24H0z" fill="none"/>
-        </svg>}
-          onClick={onClick}
-        />
-      </Drawer>
-    );
-  }
+  return (
+    <Drawer
+      docked={false}
+      width={200}
+      open={open}
+      onRequestChange={onRequestChange}
+    >
+      <MenuItem
+        primaryText={'Clear List'}
+        leftIcon={clearIcon}
+        onClick={onClearClick}
+      />
+    </Drawer>
+  );
 }
 
 export default LazyDrawer;
--- src/containers/header/LazyDrawerContainer.js
+++ src/containers/header/LazyDrawerContainer.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import type { State } from 'store';
-import { HeaderActions } from 'store/actionCreators';
+import { HeaderActions, ContentsActions } from 'store/actionCreators';
 import LazyDrawer from 'components/header/LazyDrawer';
+import IndexedDB from 'lib/IndexedDB';
 
 type Props = {
   open: boolean
 }
 
 class LazyDrawerContainer extends Component<Props> {
+  componentDidMount() {
+    let frameCount = 0;
+    const open = () => (frameCount++ > 0) ? HeaderActions.showDrawer() : requestAnimationFrame(open);
+    requestAnimationFrame(open);
+  }
+
+  handleClear = () => {
+    IndexedDB.removeAll();
+
+    ContentsActions.clearNotTodoList();
+    HeaderActions.hideDrawer();
+  }
+
   render() {
-    const { open } = this.props
+    const { open } = this.props;
+    const { handleClear } = this;
+
     return (
       <LazyDrawer
         open={open}
         onRequestChange={open => HeaderActions.setDrawer(open)}
-        onClick={() => HeaderActions.hideDrawer()}
-        onMounted={() => HeaderActions.showDrawer()}
+        onClearClick={handleClear}
       />
     );
   }
